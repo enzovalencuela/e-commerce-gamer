d371897c75e0f10a2b1fbe831ecd8e164c3b4545
enzovalencuela <esilvavalencuela@gmail.com>
2026-01-20 08:49:07 -0400

    fix: menu de ordenação dos produtos

--- src/components/MenuSearchSort/MenuSearchSort.tsx
+++ src/components/MenuSearchSort/MenuSearchSort.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useState } from "react";
 import type { Product } from "../../types";
 import "./MenuSearchSort.css";
+import Loading from "../Loading/Loading";
 
 interface MenuSearchSortProps {
   results: Product[];
@@ -9,38 +10,30 @@ interface MenuSearchSortProps {
 }
 
 function MenuSearchSort({ results, setResults, onClose }: MenuSearchSortProps) {
-  const menuRef = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState(false);
   const searchSort = ["Preço", "Pedidos"];
 
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        onClose();
-      }
-    }
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [onClose]);
-
   const sortSearchBy = (sortBy: string) => {
-    const newResults = [...results];
+    setLoading(true);
 
-    if (sortBy == "Preço") {
-      newResults.sort((a, b) => a.preco - b.preco);
-    } else if (sortBy === "Pedidos") {
-      newResults.sort((a, b) => (b.salesCount || 0) - (a.salesCount || 0));
-    }
+    setTimeout(() => {
+      const newResults = [...results];
 
-    setResults(newResults);
-    onClose();
+      if (sortBy == "Preço") {
+        newResults.sort((a, b) => a.preco - b.preco);
+      } else if (sortBy === "Pedidos") {
+        newResults.sort((a, b) => (b.salesCount || 0) - (a.salesCount || 0));
+      }
+
+      setResults(newResults);
+      onClose();
+      setLoading(false);
+    }, 1000);
   };
 
   return (
     <div className="menu-search-sort">
+      {loading && <Loading />}
       {searchSort.map((sortBy, index) => (
         <div key={index} onClick={() => sortSearchBy(sortBy)}>
           <p>{sortBy}</p>
